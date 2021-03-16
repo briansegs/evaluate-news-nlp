c5b961d9f3fff8b8d16e70cc66f48e02e8df429d
brian <briansegs@gmail.com>
2021-03-16 11:30:47 -0400

    feat: Add sentimentHandler

---
+++ src/client/js/sentimentHandler.js
@@ -0,0 +1,20 @@
+function getSentiment(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let sentimentText = document.getElementById('sentiment').value
+    // Client.checkForName(formText)
+    if (sentimentText === '') {
+        alert ('Please input text.')
+    } else {
+
+    console.log("::: Sentiment Submitted :::")
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
+    }
+}
+
+export { getSentiment }
