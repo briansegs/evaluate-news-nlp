8f55679a4d77e5e60a0215dca58b82bc2b1843c7
brian <briansegs@gmail.com>
2021-03-17 00:16:30 -0400

    fix: name typo

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,7 @@ import { handleSubmit } from './js/formHandler'
 import { getSentiment } from './js/sentimentHandler'
 import { addResult } from './js/resultsHandler'
 import { formatAnalysis } from './js/formatHandler'
-import { createTable } from './js/tableCreater'
+import { createTable } from './js/tableCreator'
 
 import './styles/resets.scss'
 import './styles/base.scss'
