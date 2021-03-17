38da132eb1eed8d43b4c8de9f3aa267bbf91fbb0
brian <briansegs@gmail.com>
2021-03-17 00:42:30 -0400

    feat: Add switch for tag transform

--- src/client/js/formatHandler.js
+++ src/client/js/formatHandler.js
@@ -5,6 +5,29 @@ function formatAnalysis(res) {
         'Agreement': res['agreement'],
         'Confidence': res['confidence']
     };
+
+    console.log(formated["Score tag"]);
+    switch (formated['Score tag']) {
+        case 'P':
+            formated['Score tag'] = 'Positive';
+            break;
+        case 'P+':
+            formated['Score tag'] = 'Strong Positive';
+            break;
+        case 'NEU':
+            formated['Score tag'] = 'Neutral';
+            break;
+        case 'N':
+            formated['Score tag'] = 'Negative';
+            break;
+        case 'N+':
+            formated['Score tag'] = 'Strong Negative';
+            break;
+        case 'NONE':
+            formated['Score tag'] = 'Without Sentiment';
+            break;
+    }
+
     return formated
 }
 
