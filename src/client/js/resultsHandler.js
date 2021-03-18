efdabfa5e7355278b516f016185152dea82ca096
brian <briansegs@gmail.com>
2021-03-18 16:16:21 -0400

    refactor: Remove mock data

--- src/client/js/resultsHandler.js
+++ src/client/js/resultsHandler.js
@@ -1,29 +1,9 @@
-function addResult(sentimentText) {
-
-    let res = {
-        status: {
-            'code': "0",
-            'msg': "OK",
-            'credits': "1",
-            'remaining_credits': "19991"
-            },
-        'model': "general_en",
-        'score_tag': "P",
-        'agreement': "AGREEMENT",
-        'subjectivity': "SUBJECTIVE",
-        'confidence': "100",
-        'irony': "NONIRONIC",
-        'sentence_list': [{'text': sentimentText}]
-    };
-
+function addResult(res) {
     let formatedResults = Client.formatAnalysis(res);
+
     console.log('Formated Results:', formatedResults);
 
     Client.createTable(formatedResults);
-
-    document.getElementById('sentiment-results').innerHTML = JSON.stringify(res);
-
-
 }
 
 export { addResult }
