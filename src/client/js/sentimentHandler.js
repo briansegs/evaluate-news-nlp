877d763e4d2a3363e11c3f5019a6d228ed2ea7d6
brian <briansegs@gmail.com>
2021-03-18 16:17:16 -0400

    feat: Build api call

--- src/client/js/sentimentHandler.js
+++ src/client/js/sentimentHandler.js
@@ -1,19 +1,24 @@
+let baseURL = 'https://api.meaningcloud.com/sentiment-2.1';
+let apiKey = `?key=${process.env.API_KEY}`;
+let json = '&of=json&txt='
+let lang = '&lang=en'
+
 function getSentiment(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
     let sentimentText = document.getElementById('sentiment').value
-    // Client.checkForName(formText)
+
     if (sentimentText === '') {
         alert ('Please input text.')
     } else {
         console.log("::: Sentiment Submitted :::")
-    // fetch('http://localhost:8081/test')
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     document.getElementById('results').innerHTML = res.message
-    // })
-        Client.addResult(sentimentText)
+
+        let text = sentimentText.replace(/\s/g, '%20');
+        console.log(baseURL+apiKey+json+text+lang)
+
+        fetch(baseURL+apiKey+json+text+lang)
+            .then(res => res.json())
+            .then(res => Client.addResult(res));
     }
 
 }
