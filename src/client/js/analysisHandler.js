db370623b1f3c48a415d6ba73409bde6476eb2a5
brian <briansegs@gmail.com>
2021-03-19 16:11:00 -0400

    fix: Remove character

--- src/client/js/analysisHandler.js
+++ src/client/js/analysisHandler.js
@@ -2,7 +2,7 @@ const getAnalysis = async (baseURL, apiKey, json, text, lang) => {
     let res = await fetch(baseURL+apiKey+json+text+lang)
     try {
         let data = await res.json();
-        return data;
+        return data
     } catch (error) {
         console.log('error', error);
     };
