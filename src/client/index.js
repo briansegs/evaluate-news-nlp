53d9d7f6e3c85350d690843b1e87eb36477635b2
brian <briansegs@gmail.com>
2021-03-19 16:24:43 -0400

    refactor: Change name for read ability

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,10 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 import { analyzeSentiment } from './js/sentimentHandler'
-import { addToDom } from './js/resultsHandler'
+import { addToDom } from './js/tableCreator'
 import { formatAnalysis } from './js/formatHandler'
-
 import { getAnalysis } from './js/analysisHandler'
-import { postData } from './js/postData'
+import { postData } from './js/dataHandler'
 
 import './styles/resets.scss'
 import './styles/base.scss'
@@ -13,8 +12,6 @@ import './styles/_page.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 import './styles/table.scss'
-
-console.log(checkForName);
 
 alert("I EXIST")
 
