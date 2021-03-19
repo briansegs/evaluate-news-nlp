76eabbe98618079cc762d8bd6a22f969c116c08a
brian <briansegs@gmail.com>
2021-03-19 11:41:53 -0400

    feat: Add analysisHandler

---
+++ src/client/js/analysisHandler.js
@@ -0,0 +1,11 @@
+const getAnalysis = async (baseURL, apiKey, json, text, lang) => {
+    let res = await fetch(baseURL+apiKey+json+text+lang)
+    try {
+        let data = await res.json();
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    };
+}
+
+export { getAnalysis }
